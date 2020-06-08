a5b756b5f288577200992a6b9fdbc93af88a84c9
zhouzhanzhao123 <2115516135@qq.com>
2020-06-08 23:44:55 +0800

    modify bug

--- run_squad.py
+++ run_squad.py
@@ -47,11 +47,11 @@ flags.DEFINE_string(
     "The output directory where the model checkpoints will be written.")
 
 ## Other parameters
-flags.DEFINE_string("train_file", 'dataset/train.json',
+flags.DEFINE_string("train_file", 'dataset/origin/train.json',
                     "SQuAD json for training. E.g., train-v1.1.json")
 
 flags.DEFINE_string(
-    "predict_file", 'dataset/test.json',
+    "predict_file", 'dataset/origin/test.json',
     "SQuAD json for predictions. E.g., dev-v1.1.json or test-v1.1.json")
 
 flags.DEFINE_string(
