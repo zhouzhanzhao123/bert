040583ddd75e8f5cf833788f446e1b8fac3a4f8b
zhouzhanzhao123 <2115516135@qq.com>
2020-06-08 23:28:01 +0800

    modify data path

--- run_squad.py
+++ run_squad.py
@@ -39,7 +39,7 @@ flags.DEFINE_string(
     "The config json file corresponding to the pre-trained BERT model. "
     "This specifies the model architecture.")
 
-flags.DEFINE_string("vocab_file", '/model/vocab.txt',
+flags.DEFINE_string("vocab_file", '/vocab.txt',
                     "The vocabulary file that the BERT model was trained on.")
 
 flags.DEFINE_string(
@@ -47,7 +47,7 @@ flags.DEFINE_string(
     "The output directory where the model checkpoints will be written.")
 
 ## Other parameters
-flags.DEFINE_string("train_file", 'dataset/simp_decomp/train.json',
+flags.DEFINE_string("train_file", 'dataset/train.json',
                     "SQuAD json for training. E.g., train-v1.1.json")
 
 flags.DEFINE_string(
