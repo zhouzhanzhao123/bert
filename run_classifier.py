9ec4d081a7aaeedce877ac699f1507ba21f28434
zhouzhanzhao123 <2115516135@qq.com>
2020-06-20 20:17:09 +0800

    改bug

--- run_classifier.py
+++ run_classifier.py
@@ -360,7 +360,7 @@ class ColaProcessor(DataProcessor):
     examples = []
     for (i, line) in enumerate(lines):
       # Only the test set has a header
-      if set_type == "test" and i == 0:
+      if i == 0:
         continue
       guid = "%s-%s" % (set_type, i)
       if set_type == "test":
