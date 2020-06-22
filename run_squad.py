849bb1db82ad52b750a5fdaf9d9b6dd82c1758be
zhouzhanzhao123 <2115516135@qq.com>
2020-06-22 15:57:47 +0800

    末尾子句例子去掉

--- run_squad.py
+++ run_squad.py
@@ -110,7 +110,7 @@ flags.DEFINE_integer(
     "nbest_predictions.json output file.")
 
 flags.DEFINE_integer(
-    "max_answer_length", 30,#last 2 model use the length of 80
+    "max_answer_length", 60,#last 2 model use the length of 80
     "The maximum length of an answer that can be generated. This is needed "
     "because the start and end predictions are not conditioned on one another.")
 
